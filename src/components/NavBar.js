442212bff596e137b7bd79fcdebd1af0053724b3
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-09-24 12:43:23 +0300

    Completed assignment.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = ["home", "about", "projects"],
+  linkText = links.map(name => <a key={name} href={`#${name}`}>{name}</a>)
+  // console.log("link", linkText)
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+  {linkText}
+  </nav>;
 }
 
 export default NavBar;
